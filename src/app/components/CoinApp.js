2ae9d43a3616a0fbf9e67f82058726a61f226ce3
saheedt <stunnafrsh007@gmail.com>
2017-07-21 19:24:03 +0100

    Add parsing and validation logic

--- src/app/components/CoinApp.js
+++ src/app/components/CoinApp.js
@@ -9,11 +9,52 @@ export default class CoinApp extends React.Component{
 		this.state = {Error : null, Output: null};
 		this.validateInput = this.validateInput.bind(this);
 		this.handleKeyDown = this.handleKeyDown.bind(this);
+		this.coins = [{value: 200, symbol:'£2'},{value: 100, symbol:'£1'},
+						{value: 50, symbol: '50p'},{value: 20, symbol:'20p'},
+						{value: 10, symbol:'10p'}, {value: 5, symbol:'5p'},
+						{value: 2, symbol:'2p'}, {value: 1, symbol:'1p'}]
 	}
 
 	validateInput(input){
 		//TODO: Validation
+		let splitPoint = input.split(".");
+		if(splitPoint > 1 && splitPoint[0].includes('p') || splitPoint > 1 && splitPoint[1].includes('£')){
+			this.setState({
+				Error: 'valid character in the wrong position'
+			});
+		}
+		if(/[^£.p\d]/.test(input)){
+			this.setState({
+				Error: 'invalid character'
+			});
+			return;
+		}
+		if(!/\d+/g.test(input)){
+			this.setState({
+				Error: 'missing value'
+			});
+			return;
+		}
 	}
+
+	parseInput(input){
+		let parsed;
+		if(input.includes('£')){
+			parsed = input.replace(/[\£\p]+/g, '');
+			parsed = parseFloat(parsed);
+			parsed = parsed.toFixed(2) * 100;
+			//return parsed;
+		}else{
+			parsed = input.replace(/[\£\p]+/g, '');
+			parsed = parseInt(parsed);
+			//return parsed;
+		}
+	}
+
+	calculate(input){
+
+	}
+
 
 	handleKeyDown(event){
 		
