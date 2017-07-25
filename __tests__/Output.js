566a061156a205fdeb13831b12b3305428e97ebc
saheedt <stunnafrsh007@gmail.com>
2017-07-25 13:57:15 +0100

    Add tests update

--- __tests__/Output.js
+++ __tests__/Output.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow , mount } from 'enzyme';
 import Output from '../src/app/components/Output.js';
 
 describe('<Output />', ()=>{
@@ -7,6 +7,22 @@ describe('<Output />', ()=>{
 
 	test('component <li> tag', ()=>{
 		expect(wrapper.find('li')).toHaveLength(1)
+	})
+
+	test('props values', ()=>{
+		const wrapper = mount(<Output coinNeeded={12}  coinValue={'£2'} oddEven={'oddItem'} />);//key={3000} coinNeeded={12}  coinValue={'£2'} oddEven={'oddItem'}
+
+		expect(wrapper.props().coinNeeded).toEqual(12);
+		expect(wrapper.props().coinValue).toEqual('£2');
+		expect(wrapper.props().oddEven).toEqual('oddItem');
+		
+		wrapper.setProps({coinNeeded: 16});
+		wrapper.setProps({coinValue: '£1'});
+		wrapper.setProps({oddEven: 'evenItem'});
+
+		expect(wrapper.props().coinNeeded).toEqual(16);
+		expect(wrapper.props().coinValue).toEqual('£1');
+		expect(wrapper.props().oddEven).toEqual('evenItem');
 	})
 
 });
